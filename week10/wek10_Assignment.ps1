4ad12be00e584df8a1fb2f7471e8d3463a5377d9
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2023-04-05 17:47:23 -0400

    Update wek10_Assignment.ps1

--- week10/wek10_Assignment.ps1
+++ week10/wek10_Assignment.ps1
@@ -2,49 +2,33 @@
 function getservicelogs(){
 # Define the log options for the user prompt
     $logptions = @('A', 'S', 'R', 'Q')
-#ask for user input
-    
     #user input is all
     if($userInput -eq 'A') {
-
-        Get-Service
-        
+        Get-Service       
     }
-
     #User input is running
     elseif($userInput -eq 'R') {
-
-        Get-Service | Where-Object {$_.Status -eq 'Running'}
-        
+        Get-Service | Where-Object {$_.Status -eq 'Running'}        
     }
-
     #user input is stopped 
     elseif ($userInput -eq 'S') {
-
-        Get-Service | Where-Object {$_.Status -eq 'Stopped'}
-  
+        Get-Service | Where-Object {$_.Status -eq 'Stopped'}  
     }
-
 }
 while ($true){
-
+#ask for user input
     $userInput = Read-Host "A for all, S for stopped, R for running, or q to quit"
-
     #user wants to quit
     if ($userInput -eq 'Q') {
-
         break
     }
-
     #invalid option
     elseif ($useIinput -notin $logOptions) {
         Write-Host "Enter which services to view."
         write-host "Ivalid option. Please type Running, Stopped, All, or quit into the prompt"
         continue
     }
-
     else{
         getservicelogs $userInput
     }
-
 }
